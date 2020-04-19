9b78941e43efed88ff5da114f1f35933b195fdc1
banterbry <35761373+banterbry@users.noreply.github.com>
2020-04-19 12:16:20 +0800

    Changed sword stats

--- RPG/items.py
+++ RPG/items.py
@@ -3,15 +3,14 @@ import time
 
 class Item:
 	'''
-	Damage range: Base * 5/6,Base * 6/5
+	Base damage = weapon attack * player attack
+	Damage range: base* 5/6, base * 6/5
 	Character is used to distinguish between potions and weapons
 	'''
 	items = {
-	"Sword" : [5,1,'w'],
-	"Bow" : [3,5,'w'],
-	"Dagger" : [2,10,'w'], 
-	"Potion of Health" : [30,0,'p'], 	
-	"Potion of Strength" : [0,20,'p']
+	"sword" : [4,1,'w'], 
+	"health potion" : [30,0,'p'], 	
+	"strength potion" : [0,20,'p']
 	}
 	def __init__(self,name,count):
 		self.id = name
